68f2afbd22887412ca0584d66d339f0be736fd5b
nurcan <nurcancurebal@hotmail.com>
2024-07-15 21:50:35 +0300

    .env

--- index.js
+++ index.js
@@ -2,35 +2,38 @@ require("dotenv").config();
 
 // ? Node modules.
 const express = require("express");
-const mongoose = require('mongoose');
-const cors = require('cors');
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 // ? Constants.
 const PORT = process.env.PORT || 3000;
+const MONGODB_URI = process.env.MONGODB_URI;
 
 // ? Database connection and conf.
 mongoose.connection
-    .on('error', error => {
-        console.error("MongoDB", error);
-    })
-    .on('open', () => {
-        console.log("MongoDB", "Database Connected");
-    });
+  .on("error", (error) => {
+    console.error("MongoDB", error);
+  })
+  .on("open", () => {
+    console.log("MongoDB", "Database Connected");
+  });
 
-mongoose.connect('mongodb://127.0.0.1:27017/ToDock');
+mongoose.connect(MONGODB_URI);
 
 // ? Express application create.
 const app = express();
 
 // ? Middlewares.
-app.use(cors({
-    origin: '*'
-}));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 app.use(require("./routes"));
 app.use(require("./middlewares/error"));
 
 // ? Server listen.
 app.listen(PORT, () => {
-    console.log("Listening api server", `http://localhost:${PORT}`);
+  console.log("Listening api server", `http://localhost:${PORT}`);
 });

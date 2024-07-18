f6b5f462820bb125fe1528c63ac4cea13323044c
nurcan <nurcancurebal@hotmail.com>
2024-07-19 00:28:46 +0300

    md5

--- controllers/auth/signin.js
+++ controllers/auth/signin.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const md5 = require("md5");
 
 const ModelUser = require("../../models/user");
 
@@ -15,7 +16,9 @@ module.exports = async function (req, res, next) {
 
     if (!resultUser) throw new Error("User not found!");
 
-    if (body.password != resultUser.password) throw new Error("Unauthorized!");
+    const hashedPassword = md5(body.password);
+
+    if (hashedPassword != resultUser.password) throw new Error("Unauthorized!");
 
     const token = jwt.sign({ id: resultUser._id.toString() }, SECRET_KEY);
 
--- controllers/auth/signup.js
+++ controllers/auth/signup.js
@@ -1,33 +1,30 @@
+const md5 = require("md5");
+
 const ModelUser = require("../../models/user");
 
 module.exports = async function (req, res, next) {
+  try {
+    const body = req.body;
 
-    try {
+    if (!body?.username) throw new Error("Username not found!");
+    if (!body?.password) throw new Error("Password not found!");
+    if (!body?.firstname) throw new Error("Firstname not found!");
+    if (!body?.lastname) throw new Error("Lastname not found!");
+    if (!body?.birthdate) throw new Error("Birthdate not found!");
 
-        const body = req.body;
-
-        if (!body?.username) throw new Error("Username not found!");
-        if (!body?.password) throw new Error("Password not found!");
-        if (!body?.firstname) throw new Error("Firstname not found!");
-        if (!body?.lastname) throw new Error("Lastname not found!");
-        if (!body?.birthdate) throw new Error("Birthdate not found!");
-
-        const data = {
-            username: body.username,
-            password: body.password,
-            firstname: body.firstname,
-            lastname: body.lastname,
-            birthdate: body.birthdate
-        };
-
-        await ModelUser.create(data);
-
-        res.send();
-
-    } catch (error) {
-
-        error.status = 400;
-        return next(error);
-
+    const data = {
+      username: body.username,
+      password: md5(body.password),
+      firstname: body.firstname,
+      lastname: body.lastname,
+      birthdate: body.birthdate,
     };
+
+    await ModelUser.create(data);
+
+    res.send();
+  } catch (error) {
+    error.status = 400;
+    return next(error);
+  }
 };
